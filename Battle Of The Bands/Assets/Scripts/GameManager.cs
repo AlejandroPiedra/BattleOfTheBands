f935115b549a336361dff5415cf214c10893de58
Alexjandropiedra <alexrock9657@gmail.com>
2020-10-28 09:24:58 -0600

    Added Music to Gameplay

---
+++ Battle Of The Bands/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public AudioSource song;
+
+    public bool startPlaying;
+
+    public NoteScroller noteScroller;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!startPlaying)
+        {
+            if (Input.anyKeyDown)
+            {
+                startPlaying = true;
+                noteScroller.startGame = true;
+
+                song.Play();
+            }
+        }
+    }
+}
