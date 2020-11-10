75cc76f7a5bb25c8d53244abb44bf17311a569de
Alexjandropiedra <alexrock9657@gmail.com>
2020-11-10 15:31:54 -0700

    Added New Scenes

--- Battle Of The Bands/Assets/Scripts/GameScript.cs
+++ Battle Of The Bands/Assets/Scripts/GameScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameScript : MonoBehaviour
 {
@@ -9,6 +10,10 @@ public class GameScript : MonoBehaviour
     public NoteScroller noteScroller;
     public PlayerScript p1;
     public PlayerScript p2;
+    public RectTransform gameOverPanel;
+    public Text gameOverText;
+    public Button gameOverButton;
+    public int soloNoteCounter = 25;
 
 
     //Set Up Game Screen
@@ -33,26 +38,53 @@ public class GameScript : MonoBehaviour
         }
 
         //Check For a Solo
-        //if (!song.isPlaying)
-        //{
-        //    if(p1.health > p2.health)
-        //    {
-        //        //P1 Solo
-        //    }
-        //    if (p2.health > p1.health)
-        //    {
-        //        //P2 Solo
-        //    }
-        //}
+        if (!song.isPlaying)
+        {
+            if(p1.health > p2.health)
+            {
+                song.Stop();
+                noteScroller.startScroll = false;
+                Solo(p1);
+            }
+            if (p2.health > p1.health)
+            {
+                //P2 Solo
+            }
+        }
 
         //Check Win/Loss
         if(p1.health <= 0)
         {
-            
+            SetUpWin();
+            gameOverText.text = "Player 1 Rocks!!";
         }
         if (p2.health <= 0)
         {
-           
+            SetUpWin();
+            gameOverText.text = "Player 2 Rocks!!";
         }
+    }
+
+    private void Solo(PlayerScript player)
+    {
+        if(soloNoteCounter <= 0)
+        {
+            gameOverText.text = player.tag + " Rocks";
+            SetUpWin();
+        }
+        foreach (ButtonController button in player.buttons)
+        {
+            if (Input.GetKeyDown(button.keyPressed))
+            {
+                soloNoteCounter -= 1;
+            }
+        }
+    }
+
+    private void SetUpWin()
+    {
+        song.Stop();
+        noteScroller.startScroll = false;
+        gameOverPanel.gameObject.SetActive(true);
     }
 }
