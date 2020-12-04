112bffadea711610d4d3fdb315dd25b8b9284f57
Alexjandropiedra <alexrock9657@gmail.com>
2020-12-03 23:52:35 -0500

    Updated Game Scene

---
+++ Battle Of The Bands/Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    public float speed = 0.5f;
+    public Renderer renderer;
+    private void Start()
+    {
+        
+    }
+    void Update()
+    {
+        renderer.material.mainTextureOffset += new Vector2(speed * Time.deltaTime, 0.0f);
+    }
+}
