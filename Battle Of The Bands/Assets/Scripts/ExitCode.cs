650438155dc514e56e6b878a0a878074519ea5f8
Alexjandropiedra <alexrock9657@gmail.com>
2020-12-09 20:49:31 -0500

    Final FInal Update

---
+++ Battle Of The Bands/Assets/Scripts/ExitCode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitCode : MonoBehaviour
+{
+    private Button quit;
+    void Start()
+    {
+        quit = GetComponent<Button>();
+        quit.onClick.AddListener(QuitGame);
+    }
+
+    private void QuitGame()
+    {
+        Application.Quit();
+    }
+}
